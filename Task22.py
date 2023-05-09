34161bc4f16114c35ff79460117f07a3cb84cbaf
KAMaz64 <konstantin.a.mazunin@gmail.com>
2023-05-09 18:35:11 +0300

    решена задача 22

---
+++ Task22.py
@@ -0,0 +1,24 @@
+
+
+N = int(input('введите кол-во элементов 1-го массива: '))
+array_A_input = input(f'Введите через пробел {N} элементов массива A[1..N]: ').split()
+array_A = list(map(int, array_A_input))
+
+M = int(input('введите кол-во элементов 2-го массива: '))
+array_B_input = input(f'Введите через пробел {M} элементов массива B[1..M]: ').split()
+array_B = list(map(int, array_B_input))
+
+
+array_C = set(array_A).intersection(set(array_B))
+
+array_D = list(array_C)
+
+temp = 0
+for i in range(0, len(array_D)):
+    for j in range(i+1, len(array_D)):
+        if array_D[i] > array_D[j]:
+            temp = array_D[i]
+            array_D[i] = array_D[j]
+            array_D[j] = temp
+
+print(array_D)
